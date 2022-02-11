7b281cac74320e7fdb0f22a7b2b3e44931e78733
eliegaboriau <elie@opensourcepolitics.eu>
2022-02-11 10:38:44 +0100

    add logger refactor add random numbers to prevent errors

--- decidim-core/db/migrate/20220204121246_change_uppercased_nicknamed.rb
+++ decidim-core/db/migrate/20220204121246_change_uppercased_nicknamed.rb
@@ -2,8 +2,12 @@
 
 class ChangeUppercasedNicknamed < ActiveRecord::Migration[6.0]
   def up
+    logger = Logger.new($stdout)
+    logger.info("Updating users nickname ...")
+
     # list of users already changed in the process
     has_changed = []
+
     Decidim::User.find_each do |user|
       next if has_changed.include? user
       # if already downcased, don't care
@@ -13,37 +17,47 @@ class ChangeUppercasedNicknamed < ActiveRecord::Migration[6.0]
         next if has_changed.include? similar_user
         next if user == similar_user
 
-        # change his nickname to the lowercased one with -1 if it's the first, -2 if it's the second etc
-        transaction do
-          Decidim::EventsManager.publish({
-                                           event: "decidim.events.nickname_event",
-                                           event_class: Decidim::ChangeNicknameEvent,
-                                           affected_users: [similar_user],
-                                           resource: similar_user,
-                                           extra: {
-                                             old_nickname: similar_user.nickname,
-                                             new_nickname: "#{similar_user.nickname.downcase}-#{index + 1}"
-                                           }
-                                         })
-          similar_user.update!(nickname: "#{similar_user.nickname.downcase}-#{index + 1}")
-          has_changed.append(similar_user)
+        # change his nickname to the lowercased one with 5 random numbers
+        begin
+          update_user_nickname(similar_user,"#{similar_user.nickname.downcase}-#{rand(99999)}")
+        rescue ActiveRecord::RecordInvalid => e
+          logger.warn("Nickname already taken : #{e}")
+          update_user_nickname(similar_user,"#{similar_user.nickname.downcase}-#{rand(99999)}")
         end
+
+        logger.info("User similar ID : #{similar_user.id}")
+        logger.info("to #{similar_user.nickname}")
+        has_changed.append(similar_user)
       end
 
-      transaction do
-        Decidim::EventsManager.publish({
-                                         event: "decidim.events.nickname_event",
-                                         event_class: Decidim::ChangeNicknameEvent,
-                                         affected_users: [user],
-                                         resource: user,
-                                         extra: {
-                                           old_nickname: user.nickname,
-                                           new_nickname: user.nickname.downcase
-                                         }
-                                       })
-        user.update!(nickname: user.nickname.downcase)
-        has_changed.append(user)
-      end
+      update_user_nickname(user,user.nickname.downcase)
+      logger.info("User ID : #{user.id}")
+      logger.info("to #{user.nickname}")
+      has_changed.append(user)
+    end
+    logger.info("Process terminated, #{has_changed.count} users nickname have been updated.")
+  end
+
+  def send_notification_to(user, new_nickname)
+    Decidim::EventsManager.publish({
+                                     event: "decidim.events.nickname_event",
+                                     event_class: Decidim::ChangeNicknameEvent,
+                                     affected_users: [user],
+                                     resource: user,
+                                     extra: {
+                                       old_nickname: user.nickname,
+                                       new_nickname: new_nickname
+                                     }
+                                   })
+  end
+
+  def update_user_nickname(user, new_nickname)
+    if user.update!(nickname: new_nickname)
+      send_notification_to(user, new_nickname)
+      logger.warn("User updated")
+      user
+    else
+      logger.warn("An error happened")
     end
   end
 end
